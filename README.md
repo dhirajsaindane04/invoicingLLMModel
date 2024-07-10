f64545b9b149ddad0d72f0dae1a0a997edf2fba3
dhirajsaindane04 <dhiraj.kumar@vkraftsoftware.com>
2024-07-10 15:10:50 +0530

    premultiinvoiceLLM model for invoice extractor added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # multiInvoiceExtractorLLMModel
+Use this Command to run the Application
+
+1. pip install -r requirements.txt
+2. streamlit run {scriptname}.py
---
+++ app.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+import streamlit as st
+import os
+from PIL import Image, UnidentifiedImageError
+import google.generativeai as genai
+
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=api_key)
+
+def get_gemini_response(input_text, image_data, prompt):
+    model = genai.GenerativeModel('gemini-pro-vision')
+    response = model.generate_content([input_text, image_data[0], prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    if uploaded_file is not None:
+        bytes_data = uploaded_file.getvalue()
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+st.set_page_config(page_title="Gemini Image Demo")
+st.header("MultiInvoice Extractor")
+
+input_text = st.text_input("Input Prompt: ", key="input")
+
+
+uploaded_file = st.file_uploader("Choose an image or PDF...", type=["jpg", "jpeg", "png", "pdf"])
+
+
+if uploaded_file is not None:
+    try:
+        if uploaded_file.type in ["image/jpeg", "image/png", "image/jpg"]:
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image.", use_column_width=True)
+        elif uploaded_file.type == "application/pdf":
+            st.write("PDF uploaded. Ready to process.")
+    except UnidentifiedImageError:
+        st.error("The uploaded file could not be identified as an image. Please upload a valid image file.")
+
+submit = st.button("Tell me about the image")
+
+
+input_prompt = """
+               You are an expert in understanding invoices.
+               You will receive input images as invoices &
+               you will have to answer questions based on the input image.
+               """
+
+if submit:
+    try:
+        image_data = input_image_setup(uploaded_file)
+        response = get_gemini_response(input_text, image_data, input_prompt)
+        st.subheader("The Response is")
+        st.write(response)
+    except Exception as e:
+        st.error(f"Error processing the request: {e}")
